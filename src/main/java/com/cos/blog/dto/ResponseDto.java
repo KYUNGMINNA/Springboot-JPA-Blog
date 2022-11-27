5293f7a03a2443635a0f9564c7a6cf3910f440b8
KYUNGMIN <nkm4095@naver.com>
2022-11-27 22:28:37 +0900

    response status , value

--- src/main/java/com/cos/blog/dto/ResponseDto.java
+++ src/main/java/com/cos/blog/dto/ResponseDto.java
@@ -1,2 +1,19 @@
-package com.cos.blog.dto;public class ResponseDto {
+package com.cos.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResponseDto<T> {
+    //HttpStatus status;
+
+    int status; //상태 값으로 받는게 좋아서 int 로 선언
+    T data;
+
 }
