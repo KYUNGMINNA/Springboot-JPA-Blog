04fccd967ba63b1cfda1b08ead397527ce20ac00
KYUNGMIN <nkm4095@naver.com>
2022-11-28 23:01:11 +0900

    Reply DTO

--- src/main/java/com/cos/blog/model/ReplySaveRequestDto.java
+++ src/main/java/com/cos/blog/model/ReplySaveRequestDto.java
@@ -1,2 +1,14 @@
-package com.cos.blog.model;public class ReplySaveRequestDto {
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplySaveRequestDto {
+    private int  userId;
+    private int boardId;
+    private String content;
 }
