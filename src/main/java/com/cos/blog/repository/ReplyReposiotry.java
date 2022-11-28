f1164019eecc6330a0eebc754e4c84287316c8c4
KYUNGMIN <nkm4095@naver.com>
2022-11-28 23:01:49 +0900

    ReplyReposigory

--- src/main/java/com/cos/blog/repository/ReplyReposiotry.java
+++ src/main/java/com/cos/blog/repository/ReplyReposiotry.java
@@ -1,4 +1,19 @@
 package com.cos.blog.repository;
 
-public interface CommentRepository {
+import com.cos.blog.model.Reply;
+import com.cos.blog.model.ReplySaveRequestDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+
+public interface ReplyReposiotry extends JpaRepository<Reply,Integer> {
+
+    @Modifying
+    @Query(value = "INSERT INTO reply(userId,boardId,content,createDate) VALUES(?1,?2,?3,now())",nativeQuery = true)
+    int mSave(int userId,int boardId,String content); //dto의 변수들이 순서대로 ?에 들어감
+    //이렇게 미리 만들어두면 영속화하지 않아도 된다.
+    //업데이트 된 행의 개수를 리턴해 줌
+
 }
